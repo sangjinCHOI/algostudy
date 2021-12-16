161ce70a00d4addcd6eb2c9760f8fd122008df8d
Hyo Jang <ase0574@naver.com>
2021-12-16 19:27:29 +0900

    update hyojeong

---
+++ 1216/hyojeong721/4836_색칠하기/s1.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    n = int(input())
+    box = [[0] * 10 for _ in range(10)]
+    color_box = [list(map(int, input().split())) for _ in range(n)]
+    res = 0
+    for i in range(n):
+        start_x = color_box[i][0]
+        start_y = color_box[i][1]
+        end_x = color_box[i][2]
+        end_y = color_box[i][3]
+        color = color_box[i][4]
+        # 색칠영역 탐색
+        for x in range(start_x, end_x+1):
+            for y in range(start_y, end_y+1):
+                # 기존 box 색상이 없거나 본인 색이랑 같으면 본인색 대입
+                if box[x][y] == 0 or box[x][y] == color:
+                    box[x][y] = color
+                # 다른 색이 있으면 출력값 += 1
+                else:
+                    box[x][y] = 3
+                    res += 1
+
+    print('#{} {}'.format(tc, res))
---
+++ 1216/hyojeong721/4837_부분집합의 합/s1.py
@@ -0,0 +1,16 @@
+import itertools
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    n, k = map(int, input().split())
+    a = [i for i in range(1, 13)]
+    # n개의 원소를 갖는 부분집합 중 합이 k인 집합의 갯수는?
+    sub = list(itertools.combinations(a, n))
+    cnt = 0
+    for i in sub:
+        sum_list = sum(i)
+        if sum_list == k:
+            cnt += 1
+    print('#{} {}'.format(tc, cnt))
