5f0601090a17c51b34d3bf14a97ef2542426a761
sangjin <toramx5@gmail.com>
2022-03-03 23:41:02 +0900

    update 0304

---
+++ 0304/sangjin/양궁대회/s1.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+
+def solution(n, info):
+    ryan = [info[i] + 1 for i in range(11)]
+    answer = []
+    score = 0
+    num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for i in range(11):
+        for p in permutations(num, i):
+            ryan_score = 0
+            arrow = 0
+            for i in p:
+                arrow += info[i] + 1
+                if arrow > n:
+                    break
+                ryan_score += (info[i] + 1) * (10 - int(i))
+            if ryan_score >= score:
+                answer.append(p)
+    print(answer)
+    #     arrow = 0
+    #     for i in range(len(info)):
+    #         for j in range(i, len(info))
+    #             if arrow < n:
+    #                 ryan[j] = info[j] + 1
+    #                 arrow += info[j] + 1
+    #             else:
+
+    #                 break
+
+    return answer
