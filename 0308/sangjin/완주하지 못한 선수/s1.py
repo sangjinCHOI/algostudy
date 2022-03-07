04539da8dc9081b5f8cbe25534cd9cf952d1bc11
sangjin <toramx5@gmail.com>
2022-03-07 18:57:19 +0900

    update 0308

---
+++ 0308/sangjin/완주하지 못한 선수/s1.py
@@ -0,0 +1,12 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+    else:
+        answer = participant[-1]
+
+    return answer
