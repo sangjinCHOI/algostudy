ef370700164af6fc4a6e5616fbe596d3db2dd89f
hyojeong721 <ase0574@naver.com>
2022-03-16 23:57:17 +0900

    update 0317

--- 0317/hyojeong721/가장큰수/s1.py
+++ 0317/hyojeong721/가장큰수/s1.py
@@ -2,7 +2,7 @@ def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x * 3, reverse=True)  # 문제에서 원소는 1000이하라고 해서
     print(numbers)
-    return str(''.join(numbers)) # 000을 처리해주기 위해 int바꿨다가 str로 바꿈(tc11번 걸림)
+    return str(int(''.join(numbers))) # [0,0,0]을 처리해주기 위해 int바꿨다가 str로 바꿈(tc11번 걸림)
 
-numbers = [9,8,7,6,5,4,3,2,1,10,10,0,0]
+numbers = [0,0,0]
 print(solution(numbers))
