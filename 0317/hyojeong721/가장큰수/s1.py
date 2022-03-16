3da56bcedd43fd7f5c78d39b2adbc8168e752aeb
hyojeong721 <ase0574@naver.com>
2022-03-16 23:54:21 +0900

    update 0317 가장큰수

---
+++ 0317/hyojeong721/가장큰수/s1.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x * 3, reverse=True)  # 문제에서 원소는 1000이하라고 해서
+    print(numbers)
+    return str(''.join(numbers)) # 000을 처리해주기 위해 int바꿨다가 str로 바꿈(tc11번 걸림)
+
+numbers = [9,8,7,6,5,4,3,2,1,10,10,0,0]
+print(solution(numbers))
